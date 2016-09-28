77af327881d5923aeaabc2df411751c4137e50b1
Nastya <nastya.volkovinskaya@gmail.com>
2016-09-28 19:38:25 +0300

    init preloader screen

--- markup/components/preloader/preloader.js
+++ markup/components/preloader/preloader.js
@@ -62,35 +62,77 @@ export let preloader = (function () {
 
     function drawInitScreen() {
         const loader = storage.read('loadResult');
-        const ss = loader.getResult('preloader');
+        const ss = loader.getResult('new_elements');
         const clock = loader.getResult('preloaderSprite');
 
         const preloaderContainer = new c.Container().set({ name: 'preloaderContainer' });
         const preloaderCache = new c.Container().set({ name: 'preloaderCache' });
-        const preloaderBG = new c.Sprite(ss, 'bg').set({ name: 'preloaderBG' });
-        const preloaderLogo = new c.Sprite(ss, 'logo');
+        const preloaderBG = new c.Bitmap(loader.getResult('preloaderBG')).set({ name: 'preloaderBG' });
+        const preloaderBGSky = new c.Bitmap(loader.getResult('mainBGSky')).set({ name: 'preloaderBGSky' });
+        const preloaderLuchi = new c.Bitmap(loader.getResult('luchi'));
+        preloaderLuchi.set({
+            name: 'preloaderLuchi',
+            x: w / 2,
+            y: h / 2 + 150,
+            scaleX: 0.6,
+            scaleY: 0.6
+        });
+        utils.getCenterPoint(preloaderLuchi);
+
+
+        const preloaderBaraban = new c.Sprite(loader.getResult('baraban'));
+        preloaderBaraban.set({
+            name: 'preloaderBaraban',
+            x: w / 2,
+            y: h / 2 + 110,
+            framerate: 2
+        });
+        utils.getCenterPoint(preloaderBaraban);
+        preloaderBaraban.play();
+        preloaderBaraban.on('animationend', function () {
+            preloaderPlay.visible = true;
+            preloaderBaraban.gotoAndStop(6);
+            preloaderBaraban.stop();
+            const tl = new TimelineMax({repeat: -1, yoyo: true});
+            tl.to(preloaderLuchi, 12, {rotation: 360, alpha: 0.1, ease: Power1.easeInOut});
+        });
+
+        const preloaderLogo = new c.Bitmap(loader.getResult('logo'));
         preloaderLogo.set({
             name: 'preloaderLogo',
-            x: (w - preloaderLogo.getBounds().width) / 2,
-            y: 75
+            x: w / 2,
+            y: 115,
+            scaleX: 0.7,
+            scaleY: 0.7
         });
-        const preloaderPlay = new c.Sprite(ss, 'play');
+        utils.getCenterPoint(preloaderLogo);
+
+        const preloaderPlay = new c.Bitmap(loader.getResult('play'));
         preloaderPlay.set({
             name: 'preloaderPlay',
-            x: (w - preloaderPlay.getBounds().width) / 2,
-            y: 310
+            x: w / 2,
+            y: 290,
+            scaleX: 0.7,
+            scaleY: 0.7,
+            visible: false
         });
-        const preloaderSprite = new c.Sprite(clock, 'start');
-        preloaderSprite.set({
-            name: 'preloaderSprite',
-            x: (w - preloaderSprite.getBounds().width) / 2 - 100,
-            y: 150
-        });
-        preloaderSprite.paused = true;
+        utils.getCenterPoint(preloaderPlay);
 
-        preloaderCache.addChild(preloaderBG, preloaderLogo);
-        preloaderCache.cache(0, 0, w, h);
-        preloaderContainer.addChild(preloaderCache, preloaderPlay, preloaderSprite);
+        const line = new c.Bitmap(loader.getResult('fonLine')).set({
+            name: 'line',
+            x: 350
+        });
+        setTimeout( function () {
+            line.x = Math.round(Math.random() * 350);
+            TweenMax.to(line, 2, {x: line.x + 30});
+        }, 2000);
+
+        preloaderCache.addChild(preloaderBGSky, preloaderLuchi, preloaderBG, line, preloaderBaraban, preloaderLogo);
+        // preloaderCache.cache(0, 0, w, h);
+        preloaderContainer.addChild(preloaderCache, preloaderPlay);
+        preloaderContainer.on('click', function (e) {
+            e.stopPropagation();
+        });
         stage.addChildAt(preloaderContainer, stage.getChildIndex(stage.getChildByName('newPreloaderContainer')));
     }
 
@@ -134,14 +176,14 @@ export let preloader = (function () {
     function clearPreloader() {
 
         let preloaderContainer = stage.getChildByName('preloaderContainer');
-        let preloaderSprite = preloaderContainer.getChildByName('preloaderSprite');
+        // let preloaderSprite = preloaderContainer.getChildByName('preloaderSprite');
         let play = preloaderContainer.getChildByName('preloaderPlay');
         play.on('click', handlePlayClick, play, true);
 
         let newPreloaderContainer = stage.getChildByName('newPreloaderContainer');
         newPreloaderContainer.cache(0, 0, utils.width, utils.height);
         TweenMax.to(newPreloaderContainer, 0.5, {alpha: 0, onComplete: function () {
-            preloaderSprite.play();
+            // preloaderSprite.play();
             storage.changeState('loaded', true);
             events.trigger('preloader:loaded');
             stage.removeChild(newPreloaderContainer);
