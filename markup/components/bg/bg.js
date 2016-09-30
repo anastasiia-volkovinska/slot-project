4a2c051b999cb3f582b32c9b8a93eb12be0b847c
Nastya <nastya.volkovinskaya@gmail.com>
2016-09-30 17:36:16 +0300

    clouds added

--- markup/components/bg/bg.js
+++ markup/components/bg/bg.js
@@ -28,8 +28,13 @@ export let bg = (function () {
         const fgContainer = new c.Container().set({name: 'fgContainer'});
         const mainBG = new c.Bitmap(loader.getResult('mainBG')).set({name: 'mainBG'});
         const mainBGSky = new c.Bitmap(loader.getResult('mainBGSky')).set({name: 'mainBGSky'});
-        const greyBGGradient = new c.Shape();
-        greyBGGradient.graphics.beginLinearGradientFill(['#000', '#FFF'], [0, 1], 0, 0, utils.width, utils.height).drawRect(0, 0, utils.width, utils.height);
+        const greyBGGradient = new c.Shape().set({
+            name: 'greyBGGradient',
+            alpha: 0
+        });
+        greyBGGradient.graphics.beginLinearGradientFill(['#000', '#FFF'], [0, 1], 0, 0, 0, utils.height).drawRect(0, 0, utils.width, utils.height);
+
+        TweenMax.to(greyBGGradient, 45, {alpha: 0.8, repeat: -1, yoyo: true, ease: Power4.easeInOut});
         const gameBG = new c.Bitmap(loader.getResult('gameBG')).set({
             name: 'gameBG',
             x: 60, // Magic Numbers
@@ -77,6 +82,10 @@ export let bg = (function () {
         fgContainer.addChild(gameMachine);
         stage.addChildAt(bgContainer, fgContainer, 0);
 
+        addCloud();
+        addCloud();
+        addCloud();
+
         // TODO: Разобраться с кешированием бекграундов
         // TODO: Перенасти отрисовку нижних полосок меню в модуль balance
 
@@ -84,6 +93,42 @@ export let bg = (function () {
         events.trigger('bg:main');
         storage.changeState('side', 'left');
         events.trigger('bg:changeSide', 'left');
+    }
+
+    function addCloud() {
+        const loader = storage.read('loadResult');
+        const newCloud = new c.Bitmap(loader.getResult('cloud')).set({
+            name: 'newCloud',
+            y: 90
+        });
+        utils.getCenterPoint(newCloud);
+
+        let side = Math.round(Math.random()) ? 'left' : 'right';
+
+        const time = 30 + Math.random() * 15 - 7.5;
+        let delta;
+        newCloud.y = newCloud.y + Math.random() * 100 - 50;
+        newCloud.scaleX = newCloud.scaleY = Math.random() * 0.5 + 0.5;
+        if (side === 'left') {
+            newCloud.x = -420;
+            delta = 1280 + 420;
+        } else {
+            newCloud.x = 1280 + 420;
+            delta = -420;
+        }
+
+        const stage = storage.read('stage');
+        const bgContainer = stage.getChildByName('bgContainer');
+        const greyBGGradient = bgContainer.getChildByName('greyBGGradient');
+
+        bgContainer.addChildAt(newCloud, bgContainer.getChildIndex(greyBGGradient));
+
+        TweenMax.to(newCloud, time, {x: delta,
+            onComplete: function () {
+                bgContainer.removeChild(newCloud);
+                addCloud();
+            }
+        });
     }
 
     function changeSide(side) {
